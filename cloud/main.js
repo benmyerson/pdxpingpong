627e263d5bd408f90165d5a01960ba051521d586
Ben Myerson <ben.myerson@bodybuilding.com>
2015-08-05 17:28:48 -0700

    ELO rating

--- cloud/main.js
+++ cloud/main.js
@@ -16,6 +16,7 @@ var getGameLoser = function getGameLoser(game) {
 }
 
 Parse.Cloud.define("mainStats", function(request, response) {
+
     var ret = {
         totalPoints: 0,
         totalGames: 0,
@@ -55,7 +56,7 @@ Parse.Cloud.define("mainStats", function(request, response) {
     });
 });
 
-Parse.Cloud.afterSave("Game", function(request) {
+Parse.Cloud.beforeSave("Game", function(request, response) {
     var game = request.object;
     var winner = game.get('player1Score') > game.get('player2Score') ? game.get('player1') : game.get('player2');
     var winnerPoints = Math.max(game.get('player1Score'),game.get('player2Score'));
@@ -64,13 +65,14 @@ Parse.Cloud.afterSave("Game", function(request) {
 
     var Player = Parse.Object.extend("Player");
     var winnerQ = new Parse.Query(Player);
-
-    winnerQ.get(winner.id, {
-        success: function(player) {
-
+    var loserQ = new Parse.Query(Player);
+    var winner, loser;
+    winnerQ.get(winner.id).then(function(player) {
+            winner = player;
             player.increment("games");
             player.increment("wins");
             player.increment("totalPoints", winnerPoints);
+            player.increment("ratedGames");
             player.increment("opponentTotalPoints", loserPoints);
             var winPerc = player.get("wins") / player.get("games");
             var ppG = player.get("totalPoints") / player.get("games");
@@ -84,19 +86,16 @@ Parse.Cloud.afterSave("Game", function(request) {
             } else {
                 player.set("streak", 1);
             }
-            player.save();
-        },
-        error: function () {
-            console.log("couldn't find winner player");
+            return player.save();
         }
-    });
-    var loserQ = new Parse.Query(Player);
-    loserQ.get(loser.id, {
-        success: function(player) {
-
+    ).then(function() {
+        return loserQ.get(loser.id);
+    }).then(function(player) {
+            loser=player;
             player.increment("games");
             player.increment("losses");
             player.increment("totalPoints", loserPoints);
+            player.increment("ratedGames");
             player.increment("opponentTotalPoints", winnerPoints);
             var winPerc = player.get("wins") / player.get("games");
             var ppG = player.get("totalPoints") / player.get("games");
@@ -111,10 +110,92 @@ Parse.Cloud.afterSave("Game", function(request) {
                 player.set("streak", -1);
             }
 
-            player.save();
-        },
-        error: function () {
-            console.log("couldn't find loser player");
+           return player.save();
         }
+    ).then(function () {
+        updatePlayerRatings(winner, loser, game);
+        winner.save();
+        loser.save();
+
+        response.success();
+    },
+    function (err) {
+        response.error(err);
     });
 });
+
+/*
+ * For non-provisional players, adjust the player's rating based on
+ * the game outcome and the opponent's rating.
+ * https://en.wikipedia.org/wiki/Elo_rating_system#Mathematical_details
+ */
+function computeEloRating(player, opponentRating, playerWon) {
+    /*
+     * Assuming 0 points for a loss and 1 for a win, compute the
+     * expected (average) score for this player against this
+     * opponent.
+     */
+
+    var rating = player.get("rating");
+    var deltaRating = opponentRating - rating;
+
+    var ln10over400 = 5.76e-3;
+    var expected = 1/(1 + Math.exp(ln10over400 * deltaRating));
+    var actual = playerWon ? 1 : 0;
+
+    /*
+     * This "K factor" is arbitrary. It represents the largest number of points
+     * that can be gained or lost by a single game. And when two equally-rated
+     * players play, each will gain or lose half this amount.
+     */
+    var K = 40;
+    rating += K * (actual - expected);
+    player.set("rating", rating);
+}
+
+/*
+ * Performance rating for the first few games to seed a player's rating.
+ * Average of the ratings of the first N players, +400 for each
+ * win and -400 for each loss.
+ * https://en.wikipedia.org/wiki/Elo_rating_system#Performance_rating
+ */
+function computeProvisionalRating(player, opponentRating, playerWon) {
+    var games = player.get("ratedGames");
+    var rating = player.get("rating");
+
+    var delta = playerWon ? 400 : -400
+
+    rating = (games * rating + opponentRating + delta)/(games + 1);
+    player.set("rating", rating);
+}
+
+function updatePlayerRating(player, opponentRating, playerWon) {
+    /*
+     * Players are provisional until they complete this many rated
+     * games.
+     */
+    var numProvisionalGames = 6;
+
+    if (player.get("ratedGames") > numProvisionalGames) {
+        computeEloRating(player, opponentRating, playerWon);
+    }
+    else {
+        computeProvisionalRating(player, opponentRating, playerWon);
+    }
+}
+
+function updatePlayerRatings(winner, loser, game) {
+    /*
+     * For historical purposes, record the rating of each player whenever
+     * a game is saved. The rating saved is each player's rating before
+     * the game.
+     */
+    var player1 = game.get("player1");
+    var player2 = game.get("player2");
+    game.set("player1Rating", player1.get("rating"));
+    game.set("player2Rating", player2.get("rating"));
+    //game.save();
+
+    updatePlayerRating(winner, loser.get("rating"), true);
+    updatePlayerRating(loser, winner.get("rating"), false);
+}
