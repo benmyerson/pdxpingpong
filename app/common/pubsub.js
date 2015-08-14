d3d71c7ecf6345507b3bbe5b4b7a373b46c73f0d
kevin.gay <kevin.gay@bodybuilding.com>
2015-08-13 17:29:31 -0700

    feat: root.$sub subscribes to events locally and at the server
    This also handles the unregistering of events when a scope is destroyed

--- app/common/pubsub.js
+++ app/common/pubsub.js
@@ -4,16 +4,33 @@ pongApp.config(function($provide) {
         return o.__proto__ || o.constructor.prototype;
     };
 
-    $provide.decorator('$rootScope', function decorateWithPubSub($delegate) {
+    $provide.decorator('$rootScope', function decorateWithPubSub($delegate, relay) {
         var root = $delegate,
             proto = getPrototypeOf(root);
+
+        // Say whaaaat?
+        $pub.relay = relay.$pub;
 
         function $pub() {
             return root.$emit.apply(root, arguments);
         }
 
-        function $sub() {
-            var removalFn = root.$on.apply(root, arguments);
+        // Observe events locally AND at the server
+        function $sub(event, callback) {
+            var removalFns = [
+                // Listen for server events
+                relay.$sub(event, callback),
+                // Listen for local events
+                root.$on.apply(root, arguments)
+            ];
+
+            function removalFn() {
+                if (removalFns) {
+                    removalFns.forEach(function(off) { off() });
+                    removalFns = null;
+                }
+            }
+
             return this.$on('$destroy', removalFn), removalFn;
         }
 
