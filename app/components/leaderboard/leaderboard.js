d4372fde6fd90285113d839a57a4644516ba6aec
Kevin James <kevinjamesus86@gmail.com>
2015-08-25 18:48:04 -0700

    update player ranks on player.update event

--- app/components/leaderboard/leaderboard.js
+++ app/components/leaderboard/leaderboard.js
@@ -1,18 +1,5 @@
 pongApp.controller('LeaderboardController', function($scope, $location, ParseService, players, relay, util) {
 
-    var lastRating = -1;
-    var lastRank = 0;
-    angular.forEach(players, function(value, key) {
-
-        if (value.rating !== lastRating) {
-            lastRating = value.rating;
-            lastRank = key + 1;
-        }
-        value.rankNumber = lastRank;
-    });
-
-    this.players = players;
-
     function updatePlayer(id, data) {
         var player = util.findWhere(players, 'objectId', id);
         if (player) {
@@ -20,13 +7,30 @@ pongApp.controller('LeaderboardController', function($scope, $location, ParseSer
         }
     }
 
+    function updateRankings() {
+        var lastRating = -1;
+        var lastRank = 0;
+        angular.forEach(players, function(player) {
+            if (player.rating !== lastRating) {
+                lastRating = player.rating;
+                lastRank += 1;
+            }
+            player.rankNumber = lastRank;
+        });
+    }
+
     $scope.$sub('player.update', function(event, id) {
         ParseService.Player.get({
             objectId: id
         }).then(function(player) {
             updatePlayer(id, player);
+            updateRankings();
         });
     });
+
+    updateRankings();
+
+    this.players = players;
 
     this.gotoPlayer = function(playerId) {
         $location.path('player/' + playerId);
