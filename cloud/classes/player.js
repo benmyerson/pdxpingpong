fe3c3c11228e0d1795797b15bffaff92366a11a3
kevin.gay <kevin.gay@bodybuilding.com>
2015-09-04 15:56:35 -0700

    feat: classes as modules

---
+++ cloud/classes/player.js
@@ -0,0 +1,19 @@
+var Player = Parse.Object.extend('Player', {
+
+    // Instance methods
+
+    isNew: function() {
+        return +this.createdAt === +this.updatedAt;
+    }
+
+}, {
+
+    // Class methods
+
+    query: function() {
+        return new Parse.Query(this);
+    }
+
+});
+
+module.exports = Player;
