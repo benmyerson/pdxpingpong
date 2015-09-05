b9ff3e917bf65e01a7ee3f09f9b292b2103f23f4
kevin.gay <kevin.gay@bodybuilding.com>
2015-09-04 17:11:56 -0700

    refactor(main): use class modules
    Parse.Object.extend, Parse.Query, and some (should have been) instance
    logic was making to much noise for me.

--- cloud/main.js
+++ cloud/main.js
@@ -1,67 +1,55 @@
 var _ = require('underscore'),
     util = require('cloud/util'),
     rating = require('cloud/rating'),
-    relay = require('cloud/relay');
+    relay = require('cloud/relay'),
 
-var getGameWinner = function getGameWinner(game) {
-    return game.get('player1Score') >
-                game.get('player2Score') ?
-                    game.get('player1') :
-                    game.get('player2');
-};
+    classes = require('cloud/classes'),
+    Game = classes.Game,
+    Team = classes.Team,
+    Season = classes.Season,
+    Profile = classes.Profile,
+    Player = classes.Player,
+    Promise = util.Promise;
 
-var getGameLoser = function getGameLoser(game) {
-    return game.get('player1Score') <
-                game.get('player2Score') ?
-                    game.get('player1') :
-                    game.get('player2');
-};
 
-var isNewObject = function isNewObject(o) {
-    return +o.createdAt === +o.updatedAt;
-};
-
+/**
+ * Cloud Function mainStats
+ */
 Parse.Cloud.define("mainStats", function(request, response) {
 
     var ret = {
-        totalPoints: 0,
         totalGames: 0,
+        totalPoints: 0,
         lastGame: {}
     };
-    var Game = Parse.Object.extend("Game");
-    var query = new Parse.Query(Game);
 
-    // cannot iterate on a query with sort, skip, or limit :/
-    // query.descending("createdAt");
-
-    query.each(function(game) {
-        var player1Score = game.get('player1Score'),
-            player2Score = game.get('player2Score');
-
+    var statsQuery = Game.query().each(function(game) {
         ret.totalGames += 1;
-        ret.totalPoints += player1Score + player2Score;
-
-    }, {
-        success: function() {
-            query = new Parse.Query(Game);
-            query.include("player1")
-                .include("player2")
-                .descending("createdAt")
-                .first()
-                .then(function(game) {
-                    ret.lastGame = game;
-                    response.success(ret);
-                },
-                function(error) {
-                    response.error(error)
-                });
-        },
-        error: function(error) {
-            response.error(error);
-        }
+        ret.totalPoints += game.getTotalPoints();
     });
+
+    var lastGameQuery = Game.query()
+                            .include("player1")
+                            .include("player2")
+                            .descending("createdAt")
+                            .first(); // Kicks off the query
+
+    Promise.all([
+        statsQuery,
+        lastGameQuery
+    ]).spread(function(_, lastGame) {
+        ret.lastGame = lastGame;
+        response.success(ret);
+    }, function(err) {
+        response.error(err);
+    });
+
 });
 
+
+/**
+ * Game.beforeSave
+ */
 Parse.Cloud.beforeSave("Game", function(request, response) {
     var game = request.object;
     var KFactor = game.get("KFactor");
@@ -74,23 +62,18 @@ Parse.Cloud.beforeSave("Game", function(request, response) {
         return;
     }
 
-    var winner = getGameWinner(game);
-    var loser = getGameLoser(game);
-    var winnerPoints = Math.max(game.get('player1Score'), game.get('player2Score'));
-    var loserPoints = Math.min(game.get('player1Score'), game.get('player2Score'));
-
-    var Player = Parse.Object.extend("SeasonPlayer");
-    var winnerQuery = new Parse.Query(Player);
-    var loserQuery = new Parse.Query(Player);
+    var winnerPoints = game.getWinnerPoints();
+    var loserPoints = game.getLoserPoints();
 
     // run Player queries in parallel
-    util.Promise.all([
-        winnerQuery.get(winner.id),
-        loserQuery.get(loser.id)
+    Promise.all([
+        Player.query().get(
+            game.getWinner().id
+        ),
+        Player.query().get(
+            game.getLoser().id
+        )
     ]).spread(function success(winner, loser) {
-        var winPercentage,
-            pointsPerGame,
-            opponentPointsPerGame
 
         /////////////////
         // Winner updates
@@ -102,19 +85,11 @@ Parse.Cloud.beforeSave("Game", function(request, response) {
         winner.increment("ratedGames");
         winner.increment("opponentTotalPoints", loserPoints);
 
-        winPercentage = winner.get("wins") / winner.get("games");
-        pointsPerGame = winner.get("totalPoints") / winner.get("games");
-        opponentPointsPerGame = winner.get("opponentTotalPoints") / winner.get("games");
+        winner.set("winPercentage", winner.getWinPercentage());
+        winner.set("pointsPerGame", winner.getAveragePointsPerGame());
+        winner.set("opponentPointsPerGame", winner.getAverageOpponentPointsPerGame());
 
-        winner.set("winPercentage", winPercentage);
-        winner.set("pointsPerGame", pointsPerGame);
-        winner.set("opponentPointsPerGame", opponentPointsPerGame);
-
-        if (winner.get("streak") > 0) {
-            winner.increment("streak");
-        } else {
-            winner.set("streak", 1);
-        }
+        winner.updateWinningStreak();
 
         ////////////////
         // Loser updates
@@ -126,19 +101,11 @@ Parse.Cloud.beforeSave("Game", function(request, response) {
         loser.increment("ratedGames");
         loser.increment("opponentTotalPoints", winnerPoints);
 
-        winPercentage = loser.get("wins") / loser.get("games");
-        pointsPerGame = loser.get("totalPoints") / loser.get("games");
-        opponentPointsPerGame = loser.get("opponentTotalPoints") / loser.get("games");
+        loser.set("winPercentage", loser.getWinPercentage());
+        loser.set("pointsPerGame", loser.getAveragePointsPerGame());
+        loser.set("opponentPointsPerGame", loser.getAverageOpponentPointsPerGame());
 
-        loser.set("winPercentage", winPercentage);
-        loser.set("pointsPerGame", pointsPerGame);
-        loser.set("opponentPointsPerGame", opponentPointsPerGame);
-
-        if (loser.get("streak") < 0) {
-            loser.increment("streak", -1);
-        } else {
-            loser.set("streak", -1);
-        }
+        loser.updateLosingStreak();
 
         return [winner, loser]
     }).spread(function(winner, loser) {
@@ -150,7 +117,7 @@ Parse.Cloud.beforeSave("Game", function(request, response) {
 
         rating.updatePlayerRatings(winner, loser, winnerPoints, loserPoints, game);
 
-        return util.Promise.all([winner.save(), loser.save()]);
+        return Promise.all([winner.save(), loser.save()]);
     }).then(function() {
         response.success(game);
     }, function(err) {
@@ -158,6 +125,10 @@ Parse.Cloud.beforeSave("Game", function(request, response) {
     });
 });
 
+
+/**
+ * Player.beforeSave
+ */
 Parse.Cloud.beforeSave('Player', function(request, response) {
     var player = request.object;
     var rating = player.get("rating");
@@ -175,8 +146,7 @@ Parse.Cloud.beforeSave('Player', function(request, response) {
         /*
          * First look up the PDX team.
          */
-        var Team = Parse.Object.extend("Team");
-        var teamQuery = new Parse.Query(Team);
+        var teamQuery = Team.query();
         teamQuery.equalTo("name", "PDX");
         teamQuery.limit(1);
         teamQuery.find({
@@ -212,34 +182,15 @@ Parse.Cloud.beforeSave('Player', function(request, response) {
     response.success();
 });
 
-Parse.Cloud.afterSave('Game', function(request) {
-    var Game = Parse.Object.extend("Game"),
-        gameQuery = new Parse.Query(Game);
-    gameQuery.get(request.object.id).then(function(game) {
-        var gameId = game.id,
-            verb = isNewObject(game) ? 'create' : 'update';
-        relay.publish('game.' + verb, gameId);
-    });
-});
 
-Parse.Cloud.afterSave('Player', function(request) {
-    var Player = Parse.Object.extend("Player"),
-        playerQuery = new Parse.Query(Player);
-    playerQuery.get(request.object.id).then(function(player) {
-        var playerId = player.id,
-            verb = isNewObject(player) ? 'create' : 'update';
-        relay.publish('player.' + verb, playerId);
-    });
-});
-
-
+/**
+ * Season.beforeSave
+ */
 Parse.Cloud.beforeSave('Season', function(request) {
     var season = request.object;
-    var Profile = new Parse.Object.extend("Profile"),
-        Player = new Parse.Object.extend("Player"),
-        allProfiles = new Parse.Query(Profile);
+    var allProfiles = Profile.query();
 
-    if (isNewObject(season)) {
+    if (season.isNew()) {
         allProfiles.each(function(profile) {
             var seasonPlayer = new Player();
             seasonPlayer.set("season", season);
@@ -261,4 +212,46 @@ Parse.Cloud.beforeSave('Season', function(request) {
         });
     }
 
+});
+
+
+/**
+ * Game.afterSave
+ */
+Parse.Cloud.afterSave('Game', function(request) {
+    var game = request.object,
+        id = game.id;
+
+    Game.query().get(id).then(function(game) {
+        var verb = game.isNew() ? 'create' : 'update';
+        relay.publish('game.' + verb, id);
+    });
+});
+
+
+/**
+ * Player.afterSave
+ */
+Parse.Cloud.afterSave('Player', function(request) {
+    var player = request.object,
+        id = player.id;
+
+    Player.query().get(id).then(function(player) {
+        var verb = player.isNew() ? 'create' : 'update';
+        relay.publish('player.' + verb, id);
+    });
+});
+
+
+/**
+ * Season.afterSave
+ */
+Parse.Cloud.afterSave('Season', function(request) {
+    var season = request.object,
+        id = season.id;
+
+    Season.query().get(id).then(function(season) {
+        var verb = season.isNew() ? 'create' : 'update';
+        relay.publish('season.' + verb, id);
+    });
 });
